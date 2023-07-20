83f376c136698687400a2b7b3d8bb64bb19ca26a
YsisC <ysisclongart@gmail.com>
2023-07-20 13:17:19 -0400

    link

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,43 +1,67 @@
-import React from 'react'
-import css from './Portfolio.module.scss'
-import { motion } from 'framer-motion'
-import { fadeIn, staggerChildren } from '../../utils/motion'
-
+import React from "react";
+import css from "./Portfolio.module.scss";
+import { motion } from "framer-motion";
+import { fadeIn, staggerChildren } from "../../utils/motion";
 
 export default function Portfolio() {
-    return (
-        <motion.section
-        variants={staggerChildren}
-        initial='hidden'
-        whileInView={"show"}
-        viewport={{once: false, amount: 0.25}}
-        className={`paddings ${css.wrapper} `}>
-            <a  className="anchor" id='portfolio'></a>
-            <div className={`innerWidth flexCenter ${css.container} `}>
-                <div className={`flexCenter ${css.headding}`}>
-                    <div>
-                        <span className='primaryText'>My Latest Works</span>
-                        <p style={{ marginTop: "10px" }}>Perfect solution</p>
-                    </div>
-                   <a href='https://github.com/YsisC?tab=repositories' ><span className="secondaryText">
-                        Explore more works
-                    </span></a>
-                </div>
-                {/* Image */}
-                <div className={`flexCenter ${css.showCase}`}>
-                   <motion.img  variants={fadeIn("up", "tween", .4, .6)}
-                        src="./app gif.png" alt='project' />
-                   <motion.img  variants={fadeIn("up", "tween", .5, .6)}
-                        src="./burguer queen.png" alt='project' />
-                    <motion.img variants={fadeIn("up", "tween", .7, .6)}
-                        src="./foodgram.png" alt='project' />
-                    <motion.img variants={fadeIn("up", "tween", .9, .6)}
-                        src="./mdlink.png" alt='project' />
-
-                </div>
-
-
-            </div>
-        </motion.section>
-    )
+  const handleImageClick = (url) => {
+    window.location.href = url;
+  };
+  return (
+    <motion.section
+      variants={staggerChildren}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.25 }}
+      className={`paddings ${css.wrapper} `}
+    >
+      <a className="anchor" id="portfolio"></a>
+      <div className={`innerWidth flexCenter ${css.container} `}>
+        <div className={`flexCenter ${css.headding}`}>
+          <div>
+            <span className="primaryText">My Latest Works</span>
+            <p style={{ marginTop: "10px" }}>Perfect solution</p>
+          </div>
+          <a href="https://github.com/YsisC?tab=repositories">
+            <span className="secondaryText">Explore more works</span>
+          </a>
+        </div>
+        {/* Image */}
+        <div className={`flexCenter ${css.showCase}`}>
+          <motion.img
+            variants={fadeIn("up", "tween", 0.4, 0.6)}
+            onClick={() =>
+              handleImageClick("https://gif-apps-angular.vercel.app/")
+            }
+            src="./app gif.png"
+            alt="project"
+          />
+          <motion.img
+            variants={fadeIn("up", "tween", 0.5, 0.6)}
+            onClick={() =>
+            handleImageClick("https://dev-003-burger-queen-api-client-o7uo5f1jy-ysisc.vercel.app/login/") 
+        }
+            src="./burguer queen.png"
+            alt="project"
+          />
+          <motion.img
+            variants={fadeIn("up", "tween", 0.7, 0.6)}
+            onClick={() =>
+              handleImageClick("https://foodgram-social-network.netlify.app/")
+            }
+            src="./foodgram.png"
+            alt="project"
+          />
+          <motion.img
+            variants={fadeIn("up", "tween", 0.9, 0.6)}
+            onClick={() =>
+              handleImageClick("https://www.npmjs.com/package/ysisc-md-links")
+            }
+            src="./mdlink.png"
+            alt="project"
+          />
+        </div>
+      </div>
+    </motion.section>
+  );
 }
